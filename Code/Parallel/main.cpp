dea9a03d54601f32c7bdb56ad1b31575b60126f7
ChinniKrishna Kothapalli <chinnikrishna.ece@gmail.com>
2013-12-04 20:23:29 -0800

    C1

--- Code/Parallel/main.cpp
+++ Code/Parallel/main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
+#include <time>
+#include <assert>
 #include <pthread.h>
-
 using namespace std;
 
+//Technology Matrix
+extern unsigned int **T;
+//Processing Time Matrix
+extern unsigned int **P;
+struct timespec startTime;
+struct timespec endTime;
+//Main Function
 int main(int argc,char** argv)
 {
     //Checking for commandline arguments
     if(argc<4)
     {
         cerr<<"Usage: " << argv[0] <<" -BenchmarkName -Population -Threads " <<endl;
-    	exit(1);
+		exit(1);
     }
+	//Start Clock
+    clock_gettime(CLOCK_REALTIME, &StartTime);
+    //1. Reading Benchmarks
+    FILE* fp = fopen(argv[1],"r+");
+    ReadBenchMark(fp, &NumJobs, &NumMachines);
 }
